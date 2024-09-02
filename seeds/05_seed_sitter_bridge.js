65fa558479972b20cf89d7c15a014cda8726f32f
Daniel <dbeaulne1@gmail.com>
2024-09-02 11:53:24 -0400

    created sitter_bridge seed file

---
+++ seeds/05_seed_sitter_bridge.js
@@ -0,0 +1,23 @@
+/* PetSpace database sitter_bridge seed file */
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('sitter_bridge').del();
+  
+  // Inserts seed entries
+  await knex('sitter_bridge').insert([
+    {
+      id: 1,
+      sitter_id: await knex('sitter').where('email', 'alice.johnson@example.com').select('account_id').first().then(row => row.account_id),
+      pet_id: await knex('pet').where('name', 'Buddy').select('id').first().then(row => row.id)
+    },
+    {
+      id: 2,
+      sitter_id: await knex('sitter').where('email', 'bob.williams@example.com').select('account_id').first().then(row => row.account_id),
+      pet_id: await knex('pet').where('name', 'Whiskers').select('id').first().then(row => row.id)
+    }
+  ]);
+};
